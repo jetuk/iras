38eb70f4aa0c1afb73ad1962f9dcd475dfd9bb15
James Tomlinson <tomo.bbe@gmail.com>
2015-07-17 20:23:23 +0100

    Added setup.py to install pyiras appropriately.

---
+++ python/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pyiras',
+      version='1.0',
+      description='Utilities for working with IRAS model',
+      author='James E Tomlinson',
+      author_email='tomo.bbe@gmail.com',
+      packages=['pyiras'],
+      )
